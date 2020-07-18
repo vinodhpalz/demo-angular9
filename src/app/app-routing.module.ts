846f0176cc658edfce173505dfb0b1363d8bcab0
Vinodh Palaniappan <vinodh88@hotmail.com>
2020-07-18 21:02:33 +0530

    Routing changes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user-list', component: UserListComponent },
+  { path: 'add-user', component: AddUserComponent },
+  { path: 'register-user', component: RegisterUserComponent },
+  { path: '', redirectTo: 'user-list', pathMatch: 'full' },
+  { path: '**', redirectTo: 'user-list', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
